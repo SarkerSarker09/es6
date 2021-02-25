9e84d8ea125457c8605d1185517ae121ad0724c3
Samer Sarker Khokon <54855806+SarkerSarker09@users.noreply.github.com>
2021-02-25 13:08:46 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 
-//====== way 01
+//====== way 01 === through rest operator
 const result1 = [...str];
 console.log(`result1: ${result1}`);
 
-//====== way 02
+//====== way 02 ===== through
 const result2 = Object.assign([], str);
 console.log(`result2:  ${result2}`);
 
-//====== way 03
+//====== way 03====== through Array 
 const result3 = Array.from(str);
 console.log(`result03: ${result3}`)
